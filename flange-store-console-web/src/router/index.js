06e536289d186beb107d90649e0a85edefe84c67
MrXiao <flangelySpring@163.com>
2019-03-04 00:57:00 +0800

    商城后台管理控制台初始化，登录页面完成

---
+++ flange-store-console-web/src/router/index.js
@@ -0,0 +1,14 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+import Login from '../views/login/index.vue';
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'login',
+      component: Login
+    }
+  ]
+})
