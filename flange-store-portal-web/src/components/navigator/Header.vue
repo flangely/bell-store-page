3ea686164d18f285c0551bb65ba63d73bf7ee4b6
MrXiao <flangelySpring@163.com>
2019-04-21 17:19:13 +0800

    商城前台导航栏布局调整

--- flange-store-portal-web/src/components/navigator/Header.vue
+++ flange-store-portal-web/src/components/navigator/Header.vue
@@ -8,9 +8,10 @@
       background-color="#f5f5f5"
       text-color="#333"
       active-text-color="#ee840b">
-      <el-menu-item index="1">请登录，注册</el-menu-item>&nbsp;&nbsp;
+      <el-menu-item></el-menu-item>
+      <el-menu-item index="1" style="margin-left:6%">返回首页</el-menu-item>
       <el-submenu index="2">
-        <template slot="title">我的订单</template>
+        <template slot="title">商家入驻</template>
         <el-menu-item index="2-1">选项1</el-menu-item>
         <el-menu-item index="2-2">选项2</el-menu-item>
         <el-menu-item index="2-3">选项3</el-menu-item>
@@ -21,8 +22,11 @@
           <el-menu-item index="2-4-3">选项3</el-menu-item>
         </el-submenu>
       </el-submenu>
-      <el-menu-item index="3">购物车</el-menu-item>
-      <el-menu-item index="4">订单管理</el-menu-item>
+      <el-menu-item index="3">客服服务</el-menu-item>
+      <el-menu-item index="4" style="margin-left:40%">登录</el-menu-item>
+      <el-menu-item index="5">注册</el-menu-item>
+      <el-menu-item index="6">我的订单</el-menu-item>
+      <el-menu-item index="7">购物车</el-menu-item>
     </el-menu>
   </div>
 </template>
