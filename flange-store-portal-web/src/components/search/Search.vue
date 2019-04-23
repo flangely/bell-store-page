c023552d7c260f69c296866f9e487d20260f2779
MrXiao <flangelySpring@163.com>
2019-04-23 23:36:53 +0800

    商城首页logo替换

--- flange-store-portal-web/src/components/search/Search.vue
+++ flange-store-portal-web/src/components/search/Search.vue
@@ -1,16 +1,16 @@
 <template>
     <div class="search" style="padding:1%">
       <el-row>
-        <el-col :span="6" style="padding-left:5%">
+        <el-col :span="6" style="padding-left:7%">
           <a href="localhost:8083/test/test">
             <img
-              src="https://image4.suning.cn/uimg/cms/img/155540698836163295.png"
+              src="~@/assets/logo.png"
               style="width:190px; height:90px;"
             >
           </a>
         </el-col>
         <el-col :span="12" align="center">
-          <el-autocomplete placeholder="请输入内容" style="width:480px;padding-top: 25px"></el-autocomplete>
+          <el-autocomplete placeholder="请输入关键字" style="width:480px;padding-top: 25px"></el-autocomplete>
           <el-button type="primary" icon="el-icon-search">搜索</el-button>
         </el-col>
       </el-row>
